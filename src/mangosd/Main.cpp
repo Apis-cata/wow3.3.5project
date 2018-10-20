8847631b533a1d5787944356f242c21f792f9a89
Apis-cata <scriptdev@mail.ru>
2018-10-20 07:00:27 +0400

    eye of acherus gonna bug

--- src/mangosd/Main.cpp
+++ src/mangosd/Main.cpp
@@ -16,7 +16,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* testing rebuild script for ubuntu.*/
 /// \addtogroup mangosd Mangos Daemon
 /// @{
 /// \file
