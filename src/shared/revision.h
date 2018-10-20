081564af73288003d2246548fd6d4df8da6b4ba2
Apis-cata <scriptdev@mail.ru>
2018-10-20 04:57:08 +0400

    revision.h editing, added our commiting & version tracking

---
+++ src/shared/revision.h
@@ -0,0 +1,8 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define REVISION_ID "effd94d26970af1eab290e18605d60c99dd97847"
+ #define REVISION_DATE "2018-10-20T03:34:58+04:00"
+ #define VERSION "0.18"
+ #define VERSIONOUR "0.01Beta"
+ #define COMMITOUR "Number 0010"
+#endif // __REVISION_H__
