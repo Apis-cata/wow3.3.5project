89509d680714b8c61d245baf4fbf460627160473
Apis-cata <scriptdev@mail.ru>
2019-06-30 15:33:34 +0300

    commit

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "728b7b4b928d6da6759fd13f20d4cd86284cc899"
- #define REVISION_DATE "2018-12-17T00:09:23+03:00"
+ #define REVISION_ID "c99a26ee685fd9b0740bb5b07b6e339d374e0e0e"
+ #define REVISION_DATE "2018-12-17T00:23:56+03:00"
  #define VERSION "0.18"
 #endif // __REVISION_H__
